da3b55cac9e969c1e947a92d6a59655c00ddf285
sosuisen <hidekazu.kubota@gmail.com>
2023-12-30 11:15:34 +0900

    fix

--- Collection/Program.cs
+++ Collection/Program.cs
@@ -4,17 +4,19 @@ namespace Collection
     {
         static void Main()
         {
-            // List<string> toDoList = new();
-            // toDoList.Add("Write Monthly Report");
-            // toDoList.Add("Schedule Team Meeting");
-            // toDoList.Add("Update Project Plan");
+            /*
+            var toDoList = new List<string>();
+            toDoList.Add("Write Monthly Report");
+            toDoList.Add("Schedule Team Meeting");
+            toDoList.Add("Update Project Plan");
+            */
 
             List<string> toDoList = ["Write Monthly Report", "Schedule Team Meeting", "Update Project Plan"];
 
-            foreach (string item in toDoList)
+            foreach (var item in toDoList)
             {
                 Console.WriteLine(item);
-            }
+            }                
         }
     }
 }
