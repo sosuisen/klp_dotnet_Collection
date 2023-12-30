3c0e91228fec8f5c781ede62c3134c727ee73937
sosuisen <hidekazu.kubota@gmail.com>
2023-12-30 11:02:49 +0900

    add basics of Dictionary

--- Collection/Program.cs
+++ Collection/Program.cs
@@ -2,18 +2,49 @@ namespace Collection
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            // List<string> toDoList = new();
-            // toDoList.Add("Write Monthly Report");
-            // toDoList.Add("Schedule Team Meeting");
-            // toDoList.Add("Update Project Plan");
+            /*
+            var weatherData = new Dictionary<string, string>();
 
-            List<string> toDoList = ["Write Monthly Report", "Schedule Team Meeting", "Update Project Plan"];
+            // Add key and value
+            weatherData.Add("Paris", "Sunny, 20ÅãC");
+            // or
+            weatherData["New York"] = "Cloudy, 15ÅãC";
 
-            foreach (string item in toDoList)
+            // Get value
+            Console.WriteLine($"Weather in NY: {weatherData["New York"]}");
+
+            // Modify value
+            weatherData["New York"] = "Cloudy, 18ÅãC";
+
+            // Get value again
+            Console.WriteLine($"Weather in NY: {weatherData["New York"]}");
+
+            // Remove key and value
+            weatherData.Remove("New York");
+
+            // Throw an exception
+            // Console.WriteLine($"Weather in Paris: {weatherData["New York"]}");
+            */
+
+            var weatherData = new Dictionary<string, string>
             {
-                Console.WriteLine(item);
+                {"Paris", "Sunny, 20ÅãC"},
+                {"New York", "Cloudy, 18ÅãC"},
+                {"London", "Rainy, 15ÅãC"}
+            };
+
+            Console.Write("Enter a city name: ");
+            var city = Console.ReadLine() ?? "";
+
+            if (weatherData.TryGetValue(city, out var value))
+            {
+                Console.WriteLine($"Weather in {city}: {value}");
+            }
+            else
+            {
+                Console.WriteLine("Weather data not available for this city.");
             }
         }
     }
