73f439a74958af844c80bb43ce3c021e7e0ac695
sosuisen <hidekazu.kubota@gmail.com>
2023-12-30 11:13:39 +0900

    add practice2

--- Collection/Program.cs
+++ Collection/Program.cs
@@ -4,17 +4,28 @@ namespace Collection
     {
         static void Main()
         {
-            // List<string> toDoList = new();
-            // toDoList.Add("Write Monthly Report");
-            // toDoList.Add("Schedule Team Meeting");
-            // toDoList.Add("Update Project Plan");
+            /*
+            var toDoList = new List<string>();
+            toDoList.Add("Write Monthly Report");
+            toDoList.Add("Schedule Team Meeting");
+            toDoList.Add("Update Project Plan");
+            */
 
             List<string> toDoList = ["Write Monthly Report", "Schedule Team Meeting", "Update Project Plan"];
+
+            toDoList.Add("Review Budget");
+            toDoList.Remove("Update Project Plan");
+            toDoList.Insert(1, "Prepare Presentation");
+            // Update
+            toDoList[0] = "Write Weekly Report";
 
             foreach (string item in toDoList)
             {
                 Console.WriteLine(item);
             }
+
+            // Get
+            Console.WriteLine($"The first task is {toDoList[0]}");
         }
     }
 }
