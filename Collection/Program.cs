a226857dfee4948b5ea8cdd3d59aa4e53beeb7dc
sosuisen <hidekazu.kubota@gmail.com>
2023-12-30 10:14:21 +0900

    プロジェクト ファイルを追加します。

---
+++ Collection/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // List<string> toDoList = new List<string>();
+        // toDoList.Add("Write Monthly Report");
+        // toDoList.Add("Schedule Team Meeting");
+        // toDoList.Add("Update Project Plan");
+        List<string> toDoList = ["Write Monthly Report", "Schedule Team Meeting", "Update Project Plan"];
+
+        foreach (string item in toDoList)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
