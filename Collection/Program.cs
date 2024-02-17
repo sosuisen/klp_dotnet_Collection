2d6bf7b5c5178243ff3fb9d93b30c63efce6744f
sosuisen <hidekazu.kubota@gmail.com>
2024-02-17 14:43:34 +0900

    fix: use toplevel statement

--- Collection/Program.cs
+++ Collection/Program.cs
@@ -1,31 +1,13 @@
-namespace Collection
+List<string> toDoList = ["Write Monthly Report", "Schedule Team Meeting", "Update Project Plan"];
+
+toDoList.Add("Review Budget");
+toDoList.Remove("Update Project Plan");
+toDoList.Insert(1, "Prepare Presentation");
+toDoList[0] = "Write Weekly Report";
+
+foreach (string item in toDoList)
 {
-    class Program
-    {
-        static void Main()
-        {
-            /*
-            var toDoList = new List<string>();
-            toDoList.Add("Write Monthly Report");
-            toDoList.Add("Schedule Team Meeting");
-            toDoList.Add("Update Project Plan");
-            */
-
-            List<string> toDoList = ["Write Monthly Report", "Schedule Team Meeting", "Update Project Plan"];
-
-            toDoList.Add("Review Budget");
-            toDoList.Remove("Update Project Plan");
-            toDoList.Insert(1, "Prepare Presentation");
-            // Update
-            toDoList[0] = "Write Weekly Report";
-
-            foreach (string item in toDoList)
-            {
-                Console.WriteLine(item);
-            }
-
-            // Get
-            Console.WriteLine($"The first task is {toDoList[0]}");
-        }
-    }
+    Console.WriteLine(item);
 }
+
+Console.WriteLine($"The first task is {toDoList[0]}");
